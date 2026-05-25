330c6ada8d90bbd39d7da5a91e07d62119414f68
Alexandre <alexandresousads@gmail.com>
2026-05-25 16:08:41 -0300

    Add files via upload

---
+++ NOME_ALEXANDRE.java
@@ -0,0 +1,22 @@
+public class Person {
+	private String name; // private = restricted access
+	
+	// Getter
+	public String getName() {
+		return name;
+	}
+	
+	// Setter 
+	public void setName(String newName) {
+		this.name = newName;
+	}
+}
+ class Main {
+	public static void main(String[]args) {
+		Person myObj = new Person();
+		myObj.setName("Alexandre"); // Set the valie of the
+		System.out.println(myObj.getName());
+		
+		// Outputs "John"
+	}
+}
